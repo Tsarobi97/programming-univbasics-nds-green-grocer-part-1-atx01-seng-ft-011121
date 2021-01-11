8ddf9f5118b7c6cefe9c80a91d321f006684da50
Robert Obi <robobi97@googlemail.com>
2021-01-11 00:52:50 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def find_item_by_name_in_collection(name, collection)
-  counter = 0
+counter = 0
 while counter < collection.length
 if collection[counter][:item] == name
   return collection[counter]
