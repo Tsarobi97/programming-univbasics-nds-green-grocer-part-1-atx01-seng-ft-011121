b713ac5ff1fe5f0c7883cb7a608796ae07b83335
Robert Obi <robobi97@googlemail.com>
2021-01-11 01:05:21 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -6,8 +6,8 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   
   collection.find do |e|
-    if e[:item] == name
-       e
+    e[:item] == name
+  
      end
    end 
  end
