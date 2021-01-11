b552dde5e7bdee0a6b6d5f25372daa32e376729c
Robert Obi <robobi97@googlemail.com>
2021-01-11 01:38:22 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,17 +11,19 @@ def find_item_by_name_in_collection(name, collection)
  end
 
 def consolidate_cart(cart)
-  binding.pry
+  counter = 0
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-  count = 0 
-  empty_cart = []
-  cart.each do ||
-    if item 
-      count += 1
-    else 
+  
+  while counter < unconsolidated_cart.length
+  if unconsolidated_cart[counter]
+    count + =1
+  else
+    [:k] => 1
+  end
+end
 
 #[:count => sum_of_all_items]
 end
