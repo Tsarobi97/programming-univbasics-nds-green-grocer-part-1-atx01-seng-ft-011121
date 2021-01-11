c2ff6821075b60b58e83c618f816b68644aec658
Robert Obi <robobi97@googlemail.com>
2021-01-11 00:53:53 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,9 +5,8 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
   collection.find do |e|
-    binding.pry
-  
   end 
+    binding.pry
 end
 
 def consolidate_cart(cart)
