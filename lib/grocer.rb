291d5b56fafc0f3a49b72c6edd2b6495bc63376a
Robert Obi <robobi97@googlemail.com>
2021-01-11 01:27:24 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -17,7 +17,11 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   count = 0 
+  empty_cart = [x]
   cart.each do ||
+    if item 
+      count += 1
+    else 
 
 #[:count => sum_of_all_items]
 end
